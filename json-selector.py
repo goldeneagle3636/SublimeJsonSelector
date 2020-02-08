6989c99310cec6904ce4b31b7c8de7a84501b67e
Aaron Silverman <aaron_silverman@me.com>
2020-02-07 17:55:35 -0800

    initial json-selector files commit

---
+++ json-selector.py
@@ -0,0 +1,12 @@
+import sublime, sublime_plugin
+import re
+import string
+
+class CopyPathCommand(sublime_plugin.TextCommand): #create Webify Text Command
+  def run(self, edit):   #implement run method
+    for region in self.view.sel():  #get user selection
+      if not region.empty():  #if selection not empty then
+        s = self.view.substr(region)  #assign s variable the selected region
+        news = s.replace('<', '&lt;')
+        news = news.replace('>', '&gt;')
+        self.view.replace(edit, region, news) #replace content in view
