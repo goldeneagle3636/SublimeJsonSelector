121f4abc0851e4b8ec268433589cd2f2ce18a4f2
Aaron Silverman <aaron_silverman@me.com>
2020-04-20 22:07:32 -0700

    checkin

--- json-selector.py
+++ json-selector.py
@@ -2,7 +2,7 @@ import sublime, sublime_plugin
 import re
 import string
 
-class CopyPathCommand(sublime_plugin.TextCommand): #create Webify Text Command
+class CopypathCommand(sublime_plugin.TextCommand): #create Webify Text Command
   def run(self, edit):   #implement run method
     for region in self.view.sel():  #get user selection
       if not region.empty():  #if selection not empty then
